be18cce064942aa254da8b991572a5e6bb94c20b
byhe <henrik.bygren@engelholm.se>
2022-06-08 06:15:45 +0200

    ch 1

---
+++ src/routes/course/chapter-1/index.svelte
@@ -0,0 +1,65 @@
+<svelte:head>
+    <title>Lär dig SvelteKit</title>
+</svelte:head>
+
+<main>
+    <article>
+        <h1>Om SvelteKit</h1>
+        <p>
+            SvelteKit är ett så kallat front-end-ramverk och är en vidareutveckling av Svelte. 
+            Skillnaden är att man med SvelteKit även kan rendera på servernivå. 
+        </p>
+        <p>
+            SvelteKit är utvecklat av <a href="https://twitter.com/rich_harris">Rich Harris</a> som ett opensource-projekt. 
+            Han är numera anställd på heltid av företaget Vercel med att utveckla Svelte och SvelteKit. 
+        </p>
+        <section>
+            <h2>Vad är ett front-end-ramverk</h2>
+            <p>
+                Ett front-end-ramverk är, i vårt fall, en miljö som förenklar utveckling av dynamiska webbsidor där behovet 
+                av JavaScript är stort. Självklart kan man göra samma sak i "ren" html, css och JavaScript som med ett ramverk. 
+                Problemet är att när sidornas komplexitet ökar blir det svårt att strukturera koden. Ramverken gör det enklare 
+                och snabbare att utveckla mer strukturerat. I början upplevs det dock som tvärt om. Så är det med allt som är nytt.
+            </p>
+            <p>
+                Det bör tilläggas att Rich Harris inte vill kalla Svelte för ett ramverk utan hellre använder ordet bibliotek eller kompilator.
+            </p>
+            <h3>Olika front-end-ramverk</h3>
+            <p>Det finns en rad olika front-end-ramverk. Exempel på dessa är </p>
+            <ul>
+                <li>ReactJS</li>
+                <li>Angular</li>
+                <li>Vue</li>
+                <li>SvelteKit</li>
+            </ul>
+            <p>
+                Det idag mest kända är ReactJS följt av Vue och Angular. SvelteKit är i skrivande stund ett nytt ramverk. 
+                SvelteKit har fått stor uppmärksamhet och anses som en stark framtida kandidat inom front-end-utveckling.       
+            </p> 
+
+            <h3>Varför SvelteKit</h3>
+            <p>
+                Anledningen att jag tar upp just SvelteKit beror på att det är enkelt att snabbt komma igång med jämfört 
+                med de andra ramverken. Detta kan man naturligtvis ha olika åsikter om.
+            </p>
+            <p>
+                Om man i dagsläge satsar på att  lära sig ett front-end-ramverk för att bli attraktiv på arbetsmarknaden 
+                så gäller främst ReactJS i Sverige men även Vue och Angular i USA och andra europeiska länder. 
+                Dock är det stora likheter mellan ramverken. Det som skiljer är främst syntaxen och den bakomliggande 
+                teknik ramverken använder. Hur man logiskt strukturerar koden är likt mellan ramverken.
+            </p>
+
+            <h3>Vilka förkunskaper krävs</h3>
+            <p>Eftersom webbläsare endast förstår "ren"</p>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>JavaScript</li>
+            </ul>
+            <p>
+                är kunskaper i dessa en fördel för att kunna lära sig och förstå olika front-end-ramverk. 
+                I slutänden är det "ren" html, css och JavaScript som ramverken på olika sätt "spottar" ut.
+            </p>
+        </section>
+    </article>
+</main>
